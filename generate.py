1827a7c753c5ac47e6a49c56b0bec70eb4abbe72
sajmon <szymon17099@gmail.com>
2026-05-23 17:02:16 +0200

    Add command line arguments

--- generate.py
+++ generate.py
@@ -1,5 +1,19 @@
+from argparse import ArgumentParser
+
+
 def main():
-    print("Hello from eon-k8s!")
+    parser = ArgumentParser(
+        prog='eon-k8s-generate',
+        description='A generator for Kubernetes-based EON simulation scenarios'
+    )
+    parser.add_argument('-s', '--step',
+                        help='Time (ms) each simulation step takes',
+                        default=300)
+    parser.add_argument('-n', '--nodes',
+                        help='Number of simulated nodes',
+                        default=20)
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == "__main__":
